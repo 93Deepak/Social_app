9b45e47f71bd495f4f40cda8f72f6152026c3e9a
93deepak <deep.c8693@gmail.com>
2022-01-29 04:34:33 +0530

    new

--- social/settings.py
+++ social/settings.py
@@ -26,8 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-yh$@(^l)@o7o_$yf@basgp02h1lmxayv0)3h7sp%s!tl%t0o91'
-# SECRET_KEY = os.environ['SECRET_KEY']
+# SECRET_KEY = 'django-insecure-yh$@(^l)@o7o_$yf@basgp02h1lmxayv0)3h7sp%s!tl%t0o91'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = True
